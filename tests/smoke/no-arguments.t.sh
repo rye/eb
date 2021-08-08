fd6bd97ccf0d7b1f43d4ca4579d8a8eeb87574be
Kristofer Rye <kristofer.rye@gmail.com>
2021-08-08 11:40:40 -0500

    t/no-arguments: Use grep pipeline instead of shell if-statement
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- tests/smoke/no-arguments.t.sh
+++ tests/smoke/no-arguments.t.sh
@@ -5,7 +5,7 @@ cd "$dir"
 
 output=$(cargo run --quiet --release -- 2>&1)
 
-if [ "$output" == "Error: no command given" ];
+if echo "$output" | tail -n 1 | grep -q "Error: no command given";
 then
 	echo "$0: passed"
 	exit 0
