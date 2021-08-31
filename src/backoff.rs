87da269192e82d43a0dce96710f464cc8e9635b7
Kristofer Rye <kristofer.rye@gmail.com>
2021-08-31 17:06:23 -0500

    backoff: Add failing unit test for wait_size
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/backoff.rs
+++ src/backoff.rs
@@ -1,1 +1,19 @@
+use core::time::Duration;
 
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn wait_size_0_failures_is_zero() {
+		let distribution = rand::distributions::Uniform::new_inclusive(0.0, 1.0);
+		let mut rng = rand::thread_rng();
+
+		let slot_time = Duration::new(1, 0);
+
+		assert_eq!(
+			wait_size(&slot_time, 0_u32, &mut rng, &distribution),
+			Duration::new(0, 0)
+		);
+	}
+}
