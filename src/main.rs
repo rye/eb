7f85a44b50593b5559c7400a8de624a0b23e0971
Kristofer Rye <kristofer.rye@gmail.com>
2026-07-15 16:35:48 +0200

    Isolate command execution and argument parsing logic
    A first step towards publishing the "core" of this crate as a library,
    I wanted to first pull the actual act of running the commands out into
    a separate module. Likewise, I wanted to put the argument parsing logic
    into its own module as well.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![deny(clippy::all)]
 #![warn(clippy::cargo, clippy::pedantic, clippy::cognitive_complexity)]
 
+mod cli;
+
+use cli::command::{CommandRunner, Outcome};
 use eb::SlotTime;
 
 use rand::distr::Uniform;
@@ -8,70 +11,22 @@ use rand::distr::Uniform;
 use log::{debug, error, info, trace};
 
 use core::time::Duration;
-use std::{
-	ffi::OsString,
-	process::{Command, ExitStatus},
-	thread::sleep,
-	time::Instant,
-};
-
-fn app() -> clap::Command {
-	use clap::{Arg, Command};
-
-	Command::new(env!("CARGO_PKG_NAME"))
-		.about(env!("CARGO_PKG_DESCRIPTION"))
-		.version(env!("CARGO_PKG_VERSION"))
-		.author(env!("CARGO_PKG_AUTHORS"))
-		.arg(
-			Arg::new("max")
-				.short('x')
-				.num_args(1)
-				.allow_negative_numbers(true)
-				.help("limits the number of times command is executed"),
-		)
-		.allow_external_subcommands(true)
-}
-
-fn command(arg_matches: &clap::ArgMatches) -> Option<Command> {
-	match arg_matches.subcommand() {
-		Some((name, matches)) => {
-			let mut command = Command::new(name);
-
-			if let Some(subcommand_args) = matches.get_many::<OsString>("") {
-				for arg in subcommand_args {
-					command.arg(arg);
-				}
-			}
-
-			Some(command)
-		}
-		_ => None,
-	}
-}
-
-fn max(arg_matches: &clap::ArgMatches) -> eb::Result<Option<u32>> {
-	match arg_matches.get_one::<String>("max") {
-		None => Ok(None),
-		Some(arg) => match arg.parse() {
-			Ok(max) => Ok(Some(max)),
-			Err(e) => Err(eb::Error::InvalidMaxValue(e.to_string())),
-		},
-	}
-}
+use std::{process::Command, thread::sleep, time::Instant};
 
 fn main() -> eb::ExecutionResult {
 	#[cfg(feature = "simple_logger")]
 	simple_logger::SimpleLogger::new().init().unwrap();
 
-	let matches = app().get_matches();
+	let matches = cli::argv::app().get_matches();
 
-	let mut command: Command = command(&matches).ok_or(eb::Error::NoCommandGiven)?;
+	let command: Command = cli::argv::command(&matches).ok_or(eb::Error::NoCommandGiven)?;
+	let mut runner = CommandRunner::new(command);
 
 	let mut iterations: u32 = 0;
 	let mut slot_time: Option<SlotTime> = None;
 	let mut rng = rand::rng();
 
-	let max: Option<u32> = max(&matches)?;
+	let max: Option<u32> = cli::argv::max(&matches)?;
 
 	let distribution = Uniform::new(0.0_f32, 1.0_f32)?;
 
@@ -84,20 +39,23 @@ fn main() -> eb::ExecutionResult {
 		trace!("Starting iteration {iterations}");
 
 		let start: Instant = Instant::now();
-		let status: ExitStatus = command.status().expect("failed to execute process");
+		let outcome: Outcome = runner.attempt();
 		let elapsed: Duration = start.elapsed();
 
 		iterations += 1;
 
-		match status.code() {
-			Some(0) => {
+		match outcome {
+			Outcome::Complete(_) => {
 				info!("Child exited with status 0; finished.");
 				break Ok(());
 			}
-			Some(code) => {
+			Outcome::Retry(status) => {
+				let code = status
+					.code()
+					.expect("a Retry outcome always carries a status code");
 				info!("Child exited with status {code}");
 			}
-			None => {
+			Outcome::Fatal => {
 				error!("Child terminated by signal");
 				break Err(eb::Error::ChildProcessTerminatedWithSignal.into());
 			}
@@ -120,112 +78,5 @@ fn main() -> eb::ExecutionResult {
 		debug!("Sleeping for {}s", delay.as_secs_f64());
 
 		sleep(delay);
-	}
-}
-#[cfg(test)]
-fn t_matches_from(argv: &[&str]) -> clap::ArgMatches {
-	let app = app();
-	app.get_matches_from(argv)
-}
-
-#[cfg(test)]
-mod max {
-	use super::{max, t_matches_from};
-
-	#[test]
-	fn not_specified() {
-		let argv = ["eb", "cmd"];
-		let matches = t_matches_from(&argv);
-		let max = max(&matches);
-
-		assert_eq!(max, Ok(None));
-	}
-
-	#[test]
-	fn short_valid() {
-		let argv = ["eb", "-x", "10", "cmd"];
-		let matches = t_matches_from(&argv);
-		let max = max(&matches);
-
-		assert_eq!(max, Ok(Some(10_u32)));
-	}
-
-	#[test]
-	fn short_invalid() {
-		let argv = ["eb", "-x", "notanumber", "cmd"];
-		let matches = t_matches_from(&argv);
-		let max = max(&matches);
-
-		assert_eq!(
-			max,
-			Err(eb::Error::InvalidMaxValue(
-				"invalid digit found in string".to_string()
-			))
-		);
-	}
-}
-
-#[cfg(test)]
-mod command {
-	use super::{command, t_matches_from};
-
-	#[test]
-	fn not_specified() {
-		let argv = ["eb"];
-		let matches = t_matches_from(&argv);
-		let command = command(&matches);
-
-		assert!(command.is_none());
-	}
-
-	#[test]
-	fn no_other_args_some() {
-		let argv = ["eb", "cmd"];
-		let matches = t_matches_from(&argv);
-		let command = command(&matches);
-
-		assert!(command.is_some());
-
-		let command = command.unwrap();
-		assert_eq!(command.get_program(), "cmd");
-
-		let mut args = command.get_args();
-		assert_eq!(args.next(), None);
-	}
-
-	#[test]
-	fn some_max_pre() {
-		let argv = ["eb", "-x", "10", "cmd"];
-		let matches = t_matches_from(&argv);
-		let command = command(&matches);
-		let max = super::max(&matches);
-
-		assert_eq!(max, Ok(Some(10_u32)));
-		assert!(command.is_some());
-
-		let command = command.unwrap();
-		assert_eq!(command.get_program(), "cmd");
-
-		let mut args = command.get_args();
-		assert_eq!(args.next(), None);
-	}
-
-	#[test]
-	fn some_max_post_none() {
-		let argv = ["eb", "cmd", "-x", "10"];
-		let matches = t_matches_from(&argv);
-		let command = command(&matches);
-		let max = super::max(&matches);
-
-		assert_eq!(max, Ok(None));
-		assert!(command.is_some());
-
-		let command = command.unwrap();
-		assert_eq!(command.get_program(), "cmd");
-
-		let mut args = command.get_args();
-		assert_eq!(args.next(), Some("-x".as_ref()));
-		assert_eq!(args.next(), Some("10".as_ref()));
-		assert_eq!(args.next(), None);
 	}
 }
